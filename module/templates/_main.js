04fcd7883a6bdf8d4735b57ef3e75bf729d2e231
mhelvens <mhelvens@gmail.com>
2014-10-18 22:48:12 +0200

    Some fixes for v0.3 that I forgot in the previous commits.

--- module/templates/_main.js
+++ module/templates/_main.js
@@ -1,14 +1,7 @@
 //
-// the styling to be loaded by webpack
-//
-require('./<%= name %>.scss');
-
-//
 // RequireJS Configuration
-// (using an extra variable to stop webpack from messing with it)
 //
-var requireJs = requirejs;
-requireJs.config({
+requirejs.config({
 	paths: {
 		'domReady': '../../bower_components/requirejs-domready/domReady',
 		'jquery': '../bower_components/jquery/dist/jquery',
@@ -24,9 +17,8 @@ requireJs.config({
 
 //
 // the application itself
-// using 'requirejs' rather than 'require', to stop webpack from messing with it
 //
-requirejs(['jquery', 'bluebird', '!domReady'], function ($, P) {
+require(['jquery', 'bluebird', './<%= name %>.scss'], function ($, P) {
 	'use strict';
 
 
